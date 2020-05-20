433013d1b060b7764500f8bfc2d3afde37ea4be1
Uvneshkumar <uvneshkumar@gmail.com>
2020-05-20 15:59:56 +0530

    Cleanup, Branding, DeBloat

--- app/src/main/java/com/bigbrains/mygallery/MediaActivity.kt
+++ app/src/main/java/com/bigbrains/mygallery/MediaActivity.kt
@@ -1,9 +1,9 @@
 package com.bigbrains.mygallery
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Activity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class MediaActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
